7865bc099caf13e9303f42948cb398cab5a8f401
Ben Bryant <brbryant2639@gmail.com>
2020-06-13 09:26:38 -0500

    Added search bar format. Started logic for searched user results.

--- src/screens/Search.tsx
+++ src/screens/Search.tsx
@@ -1,18 +1,50 @@
 import * as React from "react";
 import { View } from "react-native";
 import { SearchBar, Text, Button } from "react-native-elements";
+import { IUser } from "../utils/types";
+import { apiService } from "../utils/api";
 
 export const Search = ({ navigation }: any) => {
+  const [searchItem, setSearchItem] = React.useState<string>("");
+  const [users, setUsers] = React.useState<IUser[]>([]);
+
+  const _getUsers = async () => {
+    try {
+      let users = await apiService(
+        "https://still-taiga-99815.herokuapp.com/api/members/all"
+      );
+      setUsers(users);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   React.useLayoutEffect(() => {
     navigation.setOptions({
       headerBackTitle: "Feed",
       headerBackStyle: { color: "white" },
     });
-  });
+    _getUsers();
+  }, [users]);
+
+  const updateSearch = (search: string) => {
+    setSearchItem(search);
+    let searchedUsers = users.filter(
+      (user: IUser) =>
+        (user.firstname + " " + user.lastname).indexOf(search) !== -1
+    );
+    setUsers(searchedUsers);
+  };
 
   return (
     <View>
-      <SearchBar placeholder="Search..." />
+      <SearchBar
+        placeholder="Search..."
+        onChangeText={updateSearch}
+        value={searchItem}
+        round
+        lightTheme
+      />
       <Text h1>Search Page!</Text>
     </View>
   );
